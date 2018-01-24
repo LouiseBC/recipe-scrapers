bfe8bb09e35c7694e0b0d26452f9848a607661d4
Louise Bicker Caarten <louisehmbc@gmail.com>
2018-01-24 16:40:05 +0100

    Callable script

---
+++ scrape.py
@@ -0,0 +1,8 @@
+from recipe_scrapers import scrape_me
+import sys
+
+# give the url as a string, it can be url from any site listed below
+scraped = scrape_me(sys.argv[1])
+
+print("\n".join(scraped.ingredients()))
+sys.stdout.flush()
